efb0fe2e769b060770ab65bb063f292fe95293b8
MaksimGoryachev <m.goryachev2011@yandex.ru>
2025-01-30 15:49:14 +0500

    Changed builds script.

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 # скачиваем uv и запускаем команду установки зависимостей
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install
+uv sync
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
