82c39bcd1da2c3f4343fb1fbccf16b09189d8ad3
MaksimGoryachev <m.goryachev2011@yandex.ru>
2025-01-30 14:03:59 +0500

    Changed builds script.

--- build1.sh
+++ build1.sh
@@ -3,7 +3,10 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+# скачиваем uv и запускаем команду установки зависимостей
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+uv pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
