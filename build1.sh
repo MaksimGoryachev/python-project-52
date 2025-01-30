b408a39a0053663d17449b49127050e0f30f38cf
MaksimGoryachev <m.goryachev2011@yandex.ru>
2025-01-30 12:46:06 +0500

    Changed builds script.

--- build1.sh
+++ build1.sh
@@ -2,6 +2,9 @@
 # Exit on error
 set -o errexit
 
+# скачиваем uv и запускаем команду установки зависимостей
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 uv pip install .
 
