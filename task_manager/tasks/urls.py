829d197929a11969073c3318c85cb0a862fd555f
MaksimGoryachev <m.goryachev2011@yandex.ru>
2025-02-06 18:12:29 +0500

    Added paths and classes TaskListView, TaskCreateView.

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from task_manager.tasks import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.TaskListView.as_view(), name='task'),
+    path('create/', views.TaskCreateView.as_view(), name='task_create'),
 ]
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,6 +1,32 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import CreateView, ListView
+
+from .forms import TaskForm
+from .models import Task
 
 
-def index(request):
-    return HttpResponse('tasks')
+class TaskListView(SuccessMessageMixin, ListView):
+    model = Task
+    template_name = 'tasks/task.html'
+    context_object_name = 'tasks'
+    paginate_by = 15
+    ordering = ['-created_at']
+    extra_context = {'title': _('Tasks')}
+
+
+class TaskCreateView(SuccessMessageMixin, CreateView):
+    model = Task
+    success_url = reverse_lazy('tasks')
+    success_message = _('Task created successfully.')
+    form_class = TaskForm
+    template_name = 'forms.html'
+    extra_context = {
+        'title': _('Create task'),
+        'button_text': _('Create'),
+    }
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
