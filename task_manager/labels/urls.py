91765624446858f9837770ce05746d68b3e9399a
MaksimGoryachev <m.goryachev2011@yandex.ru>
2025-02-06 17:21:14 +0500

    Added an exception to checking the string length in all paths.

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -5,4 +5,5 @@ from task_manager.labels import views
 urlpatterns = [
     path('', views.LabelListView.as_view(), name='labels'),
     path('create/', views.LabelCreateView.as_view(), name='label_create'),
+    path('<int:pk>/delete/', views.LabelDeleteView.as_view(), name='label_delete'),
 ]
