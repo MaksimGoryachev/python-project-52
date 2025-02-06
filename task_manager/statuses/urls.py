118bde9eb33c309dda1aac1b72c9611bebe4f98a
MaksimGoryachev <m.goryachev2011@yandex.ru>
2025-02-06 17:23:52 +0500

    Added paths and classes Status List View, StatusCreateView.

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from task_manager.statuses import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.StatusListView.as_view(), name='status'),
+    path('create/', views.StatusCreateView.as_view(), name='status_create'),
+
 ]
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,6 +1,28 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import CreateView, ListView
+
+from .forms import StatusForm
+from .models import Status
 
 
-def index(request):
-    return HttpResponse('statuses')
+class StatusListView(SuccessMessageMixin, ListView):
+    model = Status
+    template_name = 'statuses/status.html'
+    context_object_name = 'statuses'
+    paginate_by = 15
+    ordering = ['-created_at']
+    extra_context = {'title': _('Statuses')}
+
+
+class StatusCreateView(SuccessMessageMixin, CreateView):
+    model = Status
+    success_url = reverse_lazy('statuses')
+    success_message = _('Status created successfully.')
+    form_class = StatusForm
+    template_name = 'forms.html'
+    extra_context = {
+        'title': _('Create status'),
+        'button_text': _('Create'),
+    }
