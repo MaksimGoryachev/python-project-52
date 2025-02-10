23c0caec4d1b2b93f82568c402a022fa09c5c6d2
MaksimGoryachev <m.goryachev2011@yandex.ru>
2025-02-10 18:45:00 +0500

    Created filter.py.

---
+++ task_manager/tasks/filter.py
@@ -0,0 +1,42 @@
+from django_filters import BooleanFilter, FilterSet, ModelChoiceFilter
+from django import forms
+# from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.translation import gettext_lazy as _
+
+from ..labels.models import Labels
+from ..statuses.models import Status
+from ..users.models import User
+from .models import Task
+
+
+class TaskFilter(FilterSet):
+    status = ModelChoiceFilter(
+        queryset=Status.objects.all(),
+        label=_('Status'),
+        empty_label=_('All')
+    )
+    executor = ModelChoiceFilter(
+        queryset=User.objects.all(),
+        label=_('Executor'),
+        empty_label=_('All')
+    )
+    label = ModelChoiceFilter(
+        queryset=Labels.objects.all(),
+        label=_('Label'),
+        empty_label=_('All')
+    )
+    your_tasks = BooleanFilter(
+        label=_('Only your own tasks'),
+        required=False,
+        method='filter_your_tasks',
+        widget=forms.CheckboxInput
+    )
+
+    def filter_your_tasks(self, queryset, name, value):
+        if value:
+            return queryset.filter(author=self.request.user)
+        return queryset
+
+    class Meta:
+        model = Task
+        fields = ['status', 'executor', 'label', 'your_tasks']
