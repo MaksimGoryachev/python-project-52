72458210559d55fe3271550a1c087804c6d4ce3e
MaksimGoryachev <m.goryachev2011@yandex.ru>
2025-02-10 11:19:20 +0500

    Added Update and Delete classes, their paths and links to templates.

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -2,13 +2,13 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
-from django.views.generic import CreateView, ListView  # UpdateView, DeleteView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from .forms import MyUserCreationForm
 from .models import User
 
 
-class UserListView(SuccessMessageMixin, ListView):
+class UserListView(ListView):
     model = User
     template_name = 'users/users.html'
     context_object_name = 'users'
@@ -27,3 +27,33 @@ class UserCreateView(CreateView, SuccessMessageMixin):
         'title': _('Create user'),
         'button_text': _('Register'),
     }
+
+
+class UserUpdateView(UpdateView, SuccessMessageMixin):
+    model = User
+    form_class = MyUserCreationForm
+    success_url = reverse_lazy('users')
+    success_message = _('User is successfully updated')
+    protected_message = _("You don't have the rights to change another user.")
+    template_name = 'forms.html'
+    extra_context = {
+        'title': _('Update user'),
+        'button_text': _('Update'),
+    }
+
+
+class UserDeleteView(DeleteView, SuccessMessageMixin):
+    model = User
+    success_url = reverse_lazy('users')
+    success_message = _('User is successfully deleted')
+    protected_message = _("You don't have the rights to change another user.")
+    template_name = 'delete.html'
+    extra_context = {
+        'title': _('Delete user'),
+        'button_text': _('Yes, delete'),
+    }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = self.object.username
+        return context
