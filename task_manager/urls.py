6b12a193ca40bf45ad5293304bad266440352cee
MaksimGoryachev <m.goryachev2011@yandex.ru>
2025-02-12 14:36:35 +0500

    Description deleted.

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for task_manager project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import include, path
 
@@ -23,14 +6,13 @@ from task_manager import views
 urlpatterns = [
     path('', views.BaseView.as_view(), name='base'),
     path('index/', views.get_index, name='index'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
 
     path('labels/', include('task_manager.labels.urls')),
     path('tasks/', include('task_manager.tasks.urls')),
     path('users/', include('task_manager.users.urls')),
     path('statuses/', include('task_manager.statuses.urls')),
-
-    path('logout/', views.UserLogoutView.as_view(), name='logout'),
-    path('login/', views.UserLoginView.as_view(), name='login'),
 
     path('admin/', admin.site.urls),
 ]
