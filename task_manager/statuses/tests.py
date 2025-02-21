1accd6fc6d6a82b5f9610a7bc297a64d32222f7a
MaksimGoryachev <m.goryachev2011@yandex.ru>
2025-02-21 11:38:40 +0500

    Created statuses/test.py

--- task_manager/statuses/tests.py
+++ task_manager/statuses/tests.py
@@ -1,3 +1,23 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from ..users.models import User
+from .models import Status
+
+
+class StatusViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser',
+                                             password='123')
+        self.client.login(username='testuser', password='123')
+        self.status = Status.objects.create(name='Test Status')
+        self.login_url = reverse('login')
+
+    def test_model_name(self):
+        self.assertEqual(str(self.status), 'Test Status')
+
+    def test_get_context_data(self):
+        response = self.client.get(reverse('status_delete',
+                                           args=[self.status.id]), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['name'], self.status.name)
