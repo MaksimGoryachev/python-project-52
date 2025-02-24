f10614c84bfa95e4a975c08188a00863599b5f2a
MaksimGoryachev <m.goryachev2011@yandex.ru>
2025-02-24 19:28:11 +0500

    Added some tests.

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,7 +1,9 @@
 from django.contrib.messages import get_messages
 from django.test import TestCase
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
+from .forms import MyUserChangeForm, MyUserCreationForm
 from .models import User
 
 
@@ -14,7 +16,9 @@ class UserViewTest(TestCase):
         self.login_url = reverse('login')
         self.logout_url = reverse('logout')
         self.base_url = reverse('base')
+        self.update_url = reverse('user_update', kwargs={'pk': self.user.pk})
         self.delete_url = reverse('user_delete', kwargs={'pk': self.user.pk})
+
         self.client.login(username='testuser', password='123')
 
     def test_login(self):
@@ -29,12 +33,13 @@ class UserViewTest(TestCase):
         })
         self.assertRedirects(response, self.base_url)
         messages = list(get_messages(response.wsgi_request))
-        self.assertEqual(str(messages[0]), 'Вы залогинены')
+        self.assertEqual(str(messages[0]), _('You are logged in'))
 
     def test_successful_logout(self):
         response = self.client.get(self.logout_url, follow=True)
         messages = list(get_messages(response.wsgi_request))
-        self.assertEqual(str(messages[0]), 'Вы разлогинены')
+        self.assertTrue(messages)
+        self.assertEqual(str(messages[0]), _('You are logged out'))
 
     def test_user_full_name(self):
         self.assertEqual(str(self.user), 'Test User')
@@ -43,3 +48,117 @@ class UserViewTest(TestCase):
         response = self.client.get(self.delete_url, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['name'], self.user.username)
+
+    def test_user_can_change_own_profile(self):
+        user_data = {
+            'first_name': 'Updated',
+            'last_name': 'Name',
+            'username': 'testuser',
+            'password1': '123',
+            'password2': '123'
+        }
+        response = self.client.post(
+            self.update_url,
+            data=user_data,
+            follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, reverse('users'))
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.username, 'testuser')
+        self.assertEqual(self.user.first_name, 'Updated')
+        self.assertEqual(self.user.last_name, 'Name')
+
+    def test_user_cannot_change_others(self):
+        other_user = User.objects.create_user(username='otheruser',
+                                              password='otherpassword')
+        self.client.login(username='testuser', password='123')
+        response = self.client.post(reverse('user_update',
+                                            args=[other_user.id]))
+        messages = list(get_messages(response.wsgi_request))
+        self.assertTrue(messages)
+        self.assertEqual(str(messages[0]),
+                         _("You don't have the rights to change another user."))
+        self.assertRedirects(response, reverse('users'))
+
+    def test_clean_username_existing_user(self):
+        form_data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'username': 'testuser',
+            'password1': 'password123',
+            'password2': 'password123',
+        }
+        form = MyUserCreationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('username', form.errors)
+        # self.assertEqual(form.errors['username'],
+        #                  [_('A user with that username already exists.')])
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+
+    def test_clean_username_can_create_user(self):
+        form_data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'username': 'newuser',
+            'password1': 'password123',
+            'password2': 'password123',
+        }
+        form = MyUserCreationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.user.refresh_from_db()
+        self.assertTrue(User.objects.filter(username='newuser').exists())
+
+    def test_clean_username_change_form_existing_user(self):
+        form_data = {
+            'first_name': 'UpdateTest',
+            'last_name': 'UpdateUser',
+            'username': 'testuser',
+            'password1': 'password123',
+            'password2': 'password123',
+        }
+        form = MyUserChangeForm(data=form_data, instance=self.user)
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.first_name, 'UpdateTest')
+        self.assertEqual(self.user.last_name, 'UpdateUser')
+        self.assertEqual(self.user.username, 'testuser')
+
+    def test_user_cannot_change_to_existing_username(self):
+        User.objects.create_user(username='testuser2', password='123')
+        self.client.login(username='testuser', password='123')
+
+        form_data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'username': 'testuser2',
+            'password1': 'password123',
+            'password2': 'password123',
+        }
+
+        form = MyUserChangeForm(data=form_data, instance=self.user)
+        self.assertFalse(form.is_valid())
+        self.assertIn('username', form.errors)
+        self.assertEqual(form.errors['username'],
+                         [_('A user with that username already exists.')])
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+
+    def test_user_profile_update_with_invalid_data(self):
+        user_data = {
+            'first_name': '',
+            'last_name': '',
+            'username': 'newuser',
+            'password1': '123',
+            'password2': '123'
+        }
+        form = MyUserChangeForm(data=user_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('first_name', form.errors)
+        self.assertEqual(form.errors['first_name'],
+                         [_('Please fill in this field.')])
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+        self.assertFalse(User.objects.filter(username='newuser').exists())
+        self.assertTrue(self.user.first_name, 'Test')
+        self.assertTrue(self.user.last_name, 'User')
