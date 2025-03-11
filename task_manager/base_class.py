42dde243823028018fd44c16fc580eb9eef5ca60
MaksimGoryachev <m.goryachev2011@yandex.ru>
2025-03-11 16:32:27 +0500

    Added BaseView class.

--- task_manager/base_class.py
+++ task_manager/base_class.py
@@ -5,39 +5,35 @@ from django.views.generic import CreateView, DeleteView, UpdateView
 from .mixin import AuthRequiredMixin
 
 
-class BaseCreateView(AuthRequiredMixin, SuccessMessageMixin, CreateView):
-    template_name = 'forms.html'
-    extra_context = {'button_text': _('Create')}
+class BaseView(AuthRequiredMixin, SuccessMessageMixin):
     title = None
+    button_text = None
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.title:
             context['title'] = self.title
+        if self.button_text:
+            context['button_text'] = self.button_text
         return context
 
 
-class BaseUpdateView(AuthRequiredMixin, SuccessMessageMixin, UpdateView):
+class BaseCreateView(BaseView, CreateView):
     template_name = 'forms.html'
-    extra_context = {'button_text': _('Update')}
-    title = None
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if self.title:
-            context['title'] = self.title
-        return context
+    button_text = _('Create')
 
 
-class BaseDeleteView(AuthRequiredMixin, SuccessMessageMixin, DeleteView):
+class BaseUpdateView(BaseView, UpdateView):
+    template_name = 'forms.html'
+    button_text = _('Update')
+
+
+class BaseDeleteView(BaseView, DeleteView):
     template_name = 'delete.html'
-    extra_context = {'button_text': _('Yes, delete')}
+    button_text = _('Yes, delete')
     name_attribute = 'name'
-    title = None
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['name'] = getattr(self.object, self.name_attribute)
-        if self.title:
-            context['title'] = self.title
         return context
